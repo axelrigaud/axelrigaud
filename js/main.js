3712cc8a855402ee420e3130daf1ca9a2fc04c73
axel rigaud <axel.rigaud@gmail.com>
2016-06-04 08:40:27 +0200

    before remote

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 $(document).ready(function(){
-  $('#loader-wrapper').fadeOut();
-  setTimeout(showParagraphs, 1000);
-  function showParagraphs() {
-    $('#whatido p').each(function(index, element){
-      setTimeout(function() {
-        $(element).addClass('fade-from-top');
-      }, index*250);
+  $(window).on('load', function(){
+    $('#loader-wrapper').fadeOut();
+    $('h1').addClass('appear-from-top');
+    setTimeout(showParagraphs, 1000);
+    function showParagraphs() {
+      $('#whatido p').each(function(index, element){
+        setTimeout(function() {
+          $(element).addClass('fade-from-top');
+        }, index*250);
+      });
+    }
+  });
+
+  //ga
+  $('a').on('click', function(e){
+    trackOutboundLink($(this).attr('href'));
+  });
+
+  var trackOutboundLink = function(url) {
+    ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
+      function () {
+      document.location = url;
+      }
     });
   }
-});
+})
